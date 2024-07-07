ae32e7a8100f91a67e335a27b519d590264bfacc
ivandjoh <ivandi.djoh@gmail.com>
2024-07-07 16:00:47 +0800

    add google logo

--- components/LoginForm.tsx
+++ components/LoginForm.tsx
@@ -1,19 +1,18 @@
 "use client";
 
-// import { calSans } from "@/app/fonts";
 import { signIn } from "next-auth/react";
-import { useFormStatus } from "react-dom";
 import { Button } from "./ui/button";
 import { calSans } from "@/app/fonts";
+import Image from 'next/image';
 
 export default function LoginForm() {
   return (
-    <div className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-        <h1 className={`${calSans.className} mb-3 text-2xl dark:text-black`}>
-          Please log in to continue.
-        </h1>
-
+    <div className="relative flex items-center justify-center">
+      <div className="absolute inset-0"></div>
+      <div className="relative z-10 rounded-lg dark:bg-gray-900 p-8 shadow-lg max-w-md mx-4">
+        <h3 className={`${calSans.className} mb-6 text-2xl text-center text-black dark:text-white`}>
+          Welcome to Instaclone, Please log in to continue.
+        </h3>
         <LoginButton />
       </div>
     </div>
@@ -21,15 +20,12 @@ export default function LoginForm() {
 }
 
 function LoginButton() {
-  const { pending } = useFormStatus();
-
   return (
     <Button
-      className="mt-4 w-full"
-      variant={"secondary"}
-      aria-disabled={pending}
+      className="flex items-center justify-center w-full border border-gray-300 rounded-lg p-2 bg-white dark:bg-gray-700 text-black dark:text-white"
       onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
     >
+      <Image src="/google-logo.svg" alt="Google Logo" width={24} height={24} className="mr-2" />
       Log in with Google
     </Button>
   );
